14e03fc3564ddbd9766b5e62da7e51a8886b1433
arpitk545 <164194769+arpitk545@users.noreply.github.com>
2025-09-18 01:45:43 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
 //auth api
-export const registerAPI="http://localhost:4000/api/v1/auth/register"
-export const loginAPI ="http://localhost:4000/api/v1/auth/login"
+export const registerAPI ="https://timetable-7dbc.onrender.com/api/v1/auth/register"
+export const loginAPI ="https://timetable-7dbc.onrender.com/api/v1/auth/login"
 
 //faculty api
-export const addFacultyAPI ="http://localhost:4000/api/v1/add-faculty"
-export const getallFacultyListAPI ="http://localhost:4000/api/v1/all-faculty-list"
+export const addFacultyAPI ="https://timetable-7dbc.onrender.com/api/v1/add-faculty"
+export const getallFacultyListAPI ="https://timetable-7dbc.onrender.com/api/v1/all-faculty-list"
 
 //room api
-export const addRoomAPI ="http://localhost:4000/api/v1/add-room"
-export const getAllRoomAPI ="http://localhost:4000/api/v1/all-rooms"
+export const addRoomAPI ="https://timetable-7dbc.onrender.com/api/v1/add-room"
+export const getAllRoomAPI ="https://timetable-7dbc.onrender.com/api/v1/all-rooms"
 
 //timetable api
-export const generatetimetableAPI ="http://localhost:4000/api/v1/generate-ai"
-export const getALLTimeTableAPI ="http://localhost:4000/api/v1/get-all-timetable"
+export const generatetimetableAPI ="https://timetable-7dbc.onrender.com/api/v1/generate-ai"
+export const getALLTimeTableAPI ="https://timetable-7dbc.onrender.com/api/v1/get-all-timetable"
