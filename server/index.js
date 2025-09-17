17c30ca9a6f74e24c871707bf97019edf4a5ae06
arpitk545 <164194769+arpitk545@users.noreply.github.com>
2025-09-18 01:54:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,10 +20,16 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(cookieParser());
 // Routes
